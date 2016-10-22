11b358e413b6f020cd61b85c214384d995c0098f
Antony Ndungu <antony.ndungu.maina@gmail.com>
2016-10-22 20:48:21 +0300

    Fixing an error

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 var express = require('express');
-var app = express();
 var moment = require('moment');
+var path = require('path');
 var fs = require('fs');
+var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
+	res.status(200);
+	res.set('Content-Type', 'text/html');
 	fs.createReadStream('./views/index.html').pipe(res);
-});
+})
 
 app.get('/:timestamp', function(req, res){
+	console.log('timestamp: ' + req.params.timestamp);
 	var time = moment(req.params.timestamp, "MMMM DD, YYYY", true);
 	if(!time.isValid()){
 		time = moment.unix(req.params.timestamp);
 		if(!time.isValid()){
+			
 			res.json({
 				unix : null,
 				natural : null
